091bb91787d217df4007947d1973e7eef0a5fa24
kamiya-s-max <s.kamiya.f15@gmail.com>
2019-10-31 16:47:01 +0900

    signupコントローラにアクションを追加

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -1,2 +1,24 @@
 class SignupController < ApplicationController
+
+  def index
+  end
+
+  def step1
+  end
+
+  def step2
+  end
+
+  def step3
+  end
+
+  def step4
+  end
+
+  def step5
+  end
+
+  def complete_signup
+  end
+
 end
