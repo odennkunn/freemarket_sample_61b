2771f86107479d1a2f1edbbd8226f5ea219147b8
kuwa1 <kuromu36@yahoo.co.jp>
2019-10-31 18:56:56 +0900

    デプロイの修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -14,7 +14,6 @@ CarrierWave.configure do |config|
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
       region: 'ap-northeast-1'
   }
-
   config.fog_directory  = 'mercari61'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/mercari61'
   end
