772c675626e7eaf56d7bfe9100a39ae906077e55
tawadmasanori <tadanori1109@gmail.com>
2019-11-02 19:09:54 +0900

    レジデンステーブル作成 #2

--- db/migrate/20191102100241_create_user_residences.rb
+++ db/migrate/20191102100241_create_user_residences.rb
@@ -1,8 +1,8 @@
 class CreateUserResidences < ActiveRecord::Migration[5.2]
   def change
     create_table :user_residences do |t|
-      t.references :user, foreign_key:true
-      t.references :residense foreign_key:true
+      t.references :user, foreign_key: true
+      t.references :residense, foreign_key: true
       t.timestamps
     end
   end
