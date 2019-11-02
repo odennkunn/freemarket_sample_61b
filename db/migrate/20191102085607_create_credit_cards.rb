98dcdbfb5acd08bfd659a0c4f97d23b754d93402
kuwa1 <kuromu36@yahoo.co.jp>
2019-11-02 18:02:45 +0900

    クレジットカードテーブルの修正

--- db/migrate/20191102085607_create_credit_cards.rb
+++ db/migrate/20191102085607_create_credit_cards.rb
@@ -1,10 +1,9 @@
 class CreateCreditCards < ActiveRecord::Migration[5.2]
   def change
     create_table :credit_cards do |t|
-      t.integer :card_number, null: false, unique: true
-      t.date :expiration_date, null: false
-      t.integer :security_number, null: false
       t.references :user, foreign_key: true
+      t.string :customer_id, null: false,
+      t.string :card_id, null: false
       t.timestamps
     end
   end
