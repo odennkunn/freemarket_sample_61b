e4a2444c2e7e9a5984284c528794d80aeb0f55e2
tawadmasanori <tadanori1109@gmail.com>
2019-11-02 18:32:00 +0900

    Update 20191102092304_create_images.rb

--- db/migrate/20191102092304_create_images.rb
+++ db/migrate/20191102092304_create_images.rb
@@ -2,7 +2,7 @@ class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
       t.references :item, foreign_key: true
-      t.string :imgae, null:false
+      t.string :image, null:false
       t.timestamps
     end
   end
