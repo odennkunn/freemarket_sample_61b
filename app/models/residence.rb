e9bd2f43192d954745f7280e888883888ffe1cd8
kamiya-s-max <s.kamiya.f15@gmail.com>
2019-11-05 16:01:54 +0900

    アソシエーションを設定

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -1,2 +1,6 @@
 class Residence < ApplicationRecord
+  has_many :users, through: :user_residences
+  has_many :user_residences
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :comments, dependent: :destroy
+  has_many :items, dependent: :destroy
+  has_many :credit_cards, dependent: :destroy
+  has_many :residences, through: :user_residences, dependent: :destroy
+  has_many :user_residences, dependent: :destroy
 end
