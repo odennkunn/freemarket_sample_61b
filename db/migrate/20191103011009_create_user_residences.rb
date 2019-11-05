3ff48908294b450a6183ba8083d6db20f11d7f54
odennkunn <y.taniguchi920@gmail.com>
2019-11-05 17:58:43 +0900

    中間テーブルの削除

--- db/migrate/20191103011009_create_user_residences.rb
+++
@@ -1,9 +0,0 @@
-class CreateUserResidences < ActiveRecord::Migration[5.2]
-  def change
-    create_table :user_residences do |t|
-      t.references :user,      foreign_key: true
-      t.references :residence, foreign_key: true
-      t.timestamps
-    end
-  end
-end
