e1f00a962aa7258030110ee8cfc881ff6b3b9f47
kamiya-s-max <s.kamiya.f15@gmail.com>
2019-11-07 10:16:11 +0900

    郵便番号のバリデーションをコメントアウト

--- app/models/residence.rb
+++ app/models/residence.rb
@@ -8,8 +8,7 @@ class Residence < ApplicationRecord
   VALID_POST_CODE = /\A[0-9]{3}-[0-9]{4}\z/
 
   #address_number 空ではないか、最大8文字、フォーマットが適切か
-  validates :address_number, presence: true, length: { maximum: 8 }
-  # , format: { with: VALID_POST_CODE, message: "フォーマットが不適切です"}
+  # validates :address_number, presence: true, length: { maximum: 8 }, format: { with: VALID_POST_CODE, message: "フォーマットが不適切です"}
   #prefecture_id 空ではないか、prefecture_idに存在するものか
   validates :prefecture_id, presence: true, numericality: { only_integer: true, less_than: 49 }
   #munisipal 空ではないか、最大50文字
