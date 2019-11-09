7c06df0fa689a6eb93783981772b0103a91df8f3
kuwa1 <kuromu36@yahoo.co.jp>
2019-11-09 20:32:42 +0900

    recaptchaの記述変更

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key = ENV['RECAPTCHA_PUBLIC_KEY']
-  config.secret_key = ENV['RECAPTCHA_PRIVATE_KEY']
+  config.public_key  = Rails.application.credentials.recaptcha['RECAPTCHA_PUBLIC_KEY']
+  config.private_key = Rails.application.credentials.recaptcha['RECAPTCHA_PRIVATE_KEY']
 end
