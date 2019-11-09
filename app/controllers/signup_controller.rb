2ba1da9b050a95e9b256465f99281e9b7f22534f
kamiya-s-max <s.kamiya.f15@gmail.com>
2019-11-09 16:03:15 +0900

    不要なコメントアウトの削除

--- app/controllers/signup_controller.rb
+++ app/controllers/signup_controller.rb
@@ -12,8 +12,6 @@ class SignupController < ApplicationController
 
   def step1
     @user = User.new
-    # @user.build_residence
-    # binding.pry
   end
 
   def save_step1
@@ -28,7 +26,7 @@ class SignupController < ApplicationController
     session[:birth_year] = user_params[:birth_year]
     session[:birth_month] = user_params[:birth_month]
     session[:birth_day] = user_params[:birth_day]
-    # binding.pry
+  
     @user = User.new(
       nickname: session[:nickname],
       email: session[:email],
@@ -42,14 +40,7 @@ class SignupController < ApplicationController
       birth_month: session[:birth_month],
       birth_day: session[:birth_day]
     )
-    # @user.build_residence(
-    #   prefecture_id: "1", 
-    #   address_number: "111-1111", 
-    #   municipal: "名古屋市中区", 
-    #   address: "中央町", 
-    #   building: "柳ビル103"
-    # )
-    # binding.pry
+
     render 'signup/step1' unless @user.valid?
     unless verify_recaptcha(model: @user)
       'signup/step1'
@@ -58,47 +49,14 @@ class SignupController < ApplicationController
 
   def step2
     @user = User.new
-    # session[:nickname] = user_params[:nickname]
-    # session[:email] = user_params[:email]
-    # session[:password] = user_params[:password]
-    # session[:password_confirmation] = user_params[:password_confirmation]
-    # session[:family_name] = user_params[:family_name]
-    # session[:last_name] = user_params[:last_name]
-    # session[:kana_family_name] = user_params[:kana_family_name]
-    # session[:kana_last_name] = user_params[:kana_last_name]
-    # session[:birth_year] = user_params[:birth_year]
-    # session[:birth_month] = user_params[:birth_month]
-    # session[:birth_day] = user_params[:birth_day]
-    # session[:user_params] = user_params
-    # session[:residence_attributes_after_step1] = user_params[:residence_attributes]
-    # binding.pry
-    # @user = User.new(
-      # nickname: session[:nickname],
-      # email: session[:email],
-      # password: session[:password],
-      # password_confirmation: session[:password_confirmation],
-      # family_name: session[:family_name],
-      # last_name: session[:last_name],
-      # kana_family_name: session[:kana_family_name],
-      # kana_last_name: session[:kana_last_name],
-      # birth_year: session[:birth_year],
-      # birth_month: session[:birth_month],
-      # birth_day: session[:birth_day]
-      # session[:user_params]
-    # )
-    # @user.build_residence
-    # binding.pry
+
   end
 
   def save_step2
     session[:phone_number] = user_params[:phone_number]
-    # binding.pry
   end
 
   def step3
-    # session[:residence_attributes_after_step2] = user_params[:residence_attributes]
-    # session[:residence_attributes_after_step2].merge!(session[:residence_attributes_after_step1])
-    # binding.pry
     @user = User.new(
       family_name: session[:family_name],
       last_name: session[:last_name],
@@ -106,22 +64,14 @@ class SignupController < ApplicationController
       kana_last_name: session[:kana_last_name]   
     )
     @user.build_residence
-    # binding.pry
   end
 
   def save_step3
-    # binding.pry
     session[:family_name] = user_params[:family_name]
     session[:last_name] = user_params[:last_name]
     session[:kana_family_name] = user_params[:kana_family_name]
     session[:kana_last_name] = user_params[:kana_last_name]    
     session[:residence_attributes_step3] = user_params[:residence_attributes]
-
-    # session[:prefecture_id] = user_params[:residence_attributes][:prefecture_id]
-    # session[:address_number] = user_params[:residence_attributes][:address_number]
-    # session[:municipal] = user_params[:residence_attributes][:municipal]
-    # session[:address] = user_params[:residence_attributes][:address]
-    # session[:building] = user_params[:residence_attributes][:building]
 
     @user = User.new(
       nickname: session[:nickname],
@@ -139,39 +89,14 @@ class SignupController < ApplicationController
     )
     @user.build_residence(
       session[:residence_attributes_step3]
-      # prefecture_id: session[:prefecture_id],
-      # address_number: session[:address_number],
-      # municipal: session[:municipal],
-      # address: session[:address],
-      # building: session[:building]
     )
 
-    #binding.pry
     render 'signup/step3' unless @user.valid?
   end
 
   def step4
     @user = User.new
     @user.build_residence
-    
-    # session[:prefecture_id] = residence_params[:prefecture_id]
-    # session[:address_number] = residence_params[:address_number]
-    # session[:municipal] = residence_params[:municipal]
-    # session[:address] = residence_params[:address]
-    # session[:building] = residence_params[:building]
-    # session[:residence_attributes_after_step3] = user_params[:residence_attributes]
-    # session[:residence_attributes_after_step3].merge!(session[:residence_attributes_after_step2])
-    # session[:residence_attributes_step3] = user_params[:residence_attributes]
-    # binding.pry
-
-    # @residence = Residence.new(
-    #   user_id: @user.id,
-    #   prefecture_id: session[:prefecture_id],
-    #   address_number: session[:address_number],
-    #   municipal: session[:municipal],
-    #   address: session[:address],
-    #   building: session[:building]
-    # )
   end
 
   def create
@@ -192,28 +117,10 @@ class SignupController < ApplicationController
     )
     @user.build_residence(session[:residence_attributes_step3])
 
-    # @user.build_residence(
-    #   prefecture_id: session[:prefecture_id],
-    #   address_number: session[:address_number],
-    #   municipal: session[:municipal],
-    #   address: session[:address],
-    #   building: session[:building]
-    # )
-
     unless @user.save
       render '/signup/step1'
     end
 
-    # Payjp.api_key = ENV["PAYJP_PRIVATE_KEY"]
-    # customer = Payjp::Customer.create(card: params[:payjp_token])
-    # @card = CreditCard.new(user_id: session[:id], customer_id: customer.id, card_id: customer.default_card)
-
-    # if @card.save
-    #   session[:id] = @user.id
-    #   redirect_to complete_signup_signup_index_path
-    # else
-    #   render '/signup/step4'
-    # end
   end
 
   def pay
@@ -256,15 +163,4 @@ class SignupController < ApplicationController
       residence_attributes: [:id, :prefecture_id, :address_number, :municipal, :address, :building]
     )
   end
-
-  # def residence_params
-  #   params.require(:residence).permit(
-  #     :user_id
-  #     :prefecture_id,
-  #     :address_number,
-  #     :municipal,
-  #     :address,
-  #     :building
-  #   )
-  # end
 end
