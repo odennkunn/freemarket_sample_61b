bc8a1a4d592b1eb45caa28e5e322675bd0fa190c
kuwa1 <kuromu36@yahoo.co.jp>
2019-11-11 18:28:45 +0900

    Update recaptcha.rb

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,7 @@
-Recaptcha.configure do |config|
-  config.site_key  = ENV["RECAPTCHA_SITE_KEY"]
-  config.secret_key = ENV["RECAPTCHA_SECRET_KEY"]
-end
+# Recaptcha.configure do |config|
+#   config.site_key  = ENV["RECAPTCHA_SITE_KEY"]
+#   config.secret_key = ENV["RECAPTCHA_SECRET_KEY"]
+# end
+
++ENV['RECAPTCHA_SITE_KEY']   = Rails.application.credentials.recaptcha[:site_key]
++ENV['RECAPTCHA_SECRET_KEY'] = Rails.application.credentials.recaptcha[:secret_key]
