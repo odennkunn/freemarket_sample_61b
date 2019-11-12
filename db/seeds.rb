c29de0eca3cede7d712699e3e19004b815ddbcc6
kuwa1 <kuromu36@yahoo.co.jp>
2019-11-12 15:32:48 +0900

    IDの追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,25 @@
-User.create!(
-  nickname: 'test',
-  email: 'test@test.com',
-  password: 'aaaa1111',
-  password_confirmation: 'aaaa1111',
-  family_name: '山田',
-  last_name: '太郎',
-  kana_family_name: 'ヤマダ',
-  kana_last_name: 'タロウ',
-  birth_year: '1995',
-  birth_month: '12',
-  birth_day: '12',
+# User.create!(
+#   nickname: 'test',
+#   email: 'test@test.com',
+#   password: 'aaaa1111',
+#   password_confirmation: 'aaaa1111',
+#   family_name: '山田',
+#   last_name: '太郎',
+#   kana_family_name: 'ヤマダ',
+#   kana_last_name: 'タロウ',
+#   birth_year: '1995',
+#   birth_month: '12',
+#   birth_day: '12',
+# )
+
+Bland.create!(
+  id: "3",
+  name: "adidas",
+)
+
+
+Category.create!(
+  id: "1",
+  name: "レディース"
 )
 
