3516f83f8b5d73271c93f2f1d93cccef923c6982
tawadmasanori <tadanori1109@gmail.com>
2019-11-13 16:03:17 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,8 +21,11 @@ class ItemsController < ApplicationController
 
   def destroy
     @item = Item.find(params[:id])
-    @item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    elsif
+      render :index
+    end
   end 
   
   def pay
