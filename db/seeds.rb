1c1172b555fbbdf3423bc51cdc817c70eef32af0
kuwa1 <kuromu36@yahoo.co.jp>
2019-11-14 13:23:11 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,57 +4,57 @@ Category.create!(
   name: "メンズ",
 )
 
-Category.create!(
-  id: "3",
-  name: "ベビー・キッズ",
-)
+# Category.create!(
+#   id: "3",
+#   name: "ベビー・キッズ",
+# )
 
-Category.create!(
-  id: "4",
-  name: "インテリア・住まい・小物"
-)
+# Category.create!(
+#   id: "4",
+#   name: "インテリア・住まい・小物"
+# )
 
-Category.create!(
-  id: "5",
-  name: "本・音楽・ゲーム"
-)
+# Category.create!(
+#   id: "5",
+#   name: "本・音楽・ゲーム"
+# )
 
-Category.create!(
-  id: "6",
-  name: "おもちゃ・ホビー・グッズ"
-)
+# Category.create!(
+#   id: "6",
+#   name: "おもちゃ・ホビー・グッズ"
+# )
 
-Category.create!(
-  id: "7",
-  name: "コスメ・香水・美容"
-)
+# Category.create!(
+#   id: "7",
+#   name: "コスメ・香水・美容"
+# )
 
-Category.create!(
-  id: "8",
-  name: "家電・スマホ・カメラ"
-)
+# Category.create!(
+#   id: "8",
+#   name: "家電・スマホ・カメラ"
+# )
 
-Category.create!(
-  id: "9",
-  name: "スポーツ・レジャー"
-)
+# Category.create!(
+#   id: "9",
+#   name: "スポーツ・レジャー"
+# )
 
-Category.create!(
-  id: "10",
-  name: "ハンドメイド"
-)
+# Category.create!(
+#   id: "10",
+#   name: "ハンドメイド"
+# )
 
-Category.create!(
-  id: "11",
-  name: "チケット"
-)
+# Category.create!(
+#   id: "11",
+#   name: "チケット"
+# )
 
-Category.create!(
-  id: "12",
-  name: "自転車・オートバイ"
-)
+# Category.create!(
+#   id: "12",
+#   name: "自転車・オートバイ"
+# )
 
-Category.create!(
-  id: "13",
-  name: "その他"
-)
+# Category.create!(
+#   id: "13",
+#   name: "その他"
+# )
