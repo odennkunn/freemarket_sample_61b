5580e12b699a4f6ef1c81c7bb856a7703c352ffa
kamiya-s-max <s.kamiya.f15@gmail.com>
2019-11-14 14:30:44 +0900

    本人情報登録ページの実装

---
+++ app/controllers/residences_controller.rb
@@ -0,0 +1,21 @@
+class ResidencesController < ApplicationController
+
+  def edit
+    @residence = Residence.find(params[:id])
+  end
+
+  def update
+    residence = Residence.find(params[:id])
+    if residence.user_id == current_user.id
+      residence.update(residence_params)
+      redirect_to user_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def residence_params
+    params.require(:residence).permit(:address_number, :prefecture_id, :municipal, :address, :building)
+  end
+end
